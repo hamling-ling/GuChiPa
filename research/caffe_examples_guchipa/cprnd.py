4adf372d0630b0c063ce7035857cdb964334ca04
Nobuhiro Kuroiwa <nobu@Nobuhiro-no-MacBook-Pro.local>
2017-09-01 00:34:29 +0900

    add random file copy script

---
+++ research/caffe_examples_guchipa/cprnd.py
@@ -0,0 +1,38 @@
+import glob
+import sys
+import random
+import subprocess
+import os
+
+if len(sys.argv) < 4:
+    print("error. need two arguments")
+    print('ex) cprnd.py 3 "a/*.jpg" "b/"')
+    exit(1)
+
+num_file = int(sys.argv[1])
+dir_src = sys.argv[2]
+dir_dst = sys.argv[3]
+
+files = glob.glob(dir_src)
+
+if(len(files) < num_file):
+    print("given file number({0}) exceeds actual file number({1})"
+          .format(num_file, len(files)))
+    exit(1)
+
+if(os.path.exists(dir_dst) == False):
+    print("{0} does not exist".format(dir_dst))
+    exit(1)
+
+if(os.path.isdir(dir_dst) == False):
+    print("{0} is not a directory".format(dir_dst))
+    exit(1)
+    
+selected_files = random.sample(files, num_file)
+for f in selected_files:
+    cmd = "cp -r {0} {1}".format(f, dir_dst)
+    print(cmd)
+    subprocess.call(cmd, shell=True)
+
+print("finished")
+
