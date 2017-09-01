46adccc81de0d51bf7401136a46b36ddb159078b
Nobuhiro Kuroiwa <nobuhiro.kuroiwa@pixela.jp>
2017-09-01 20:32:35 +0900

    change image path

--- research/caffe_examples_guchipa/helloclassify.py
+++ research/caffe_examples_guchipa/helloclassify.py
@@ -1,9 +1,9 @@
 import caffe
 
-CAFFE_ROOT = '/home/pixela/GitHub/caffe'
+CAFFE_ROOT = os.getenv("CAFFE_ROOT")
 MODEL_FILE = CAFFE_ROOT + '/examples/guchipa/lenet.prototxt'
 PRETRAINED = CAFFE_ROOT + '/examples/guchipa/lenet_iter_10000.caffemodel'
-IMAGE_FILE = CAFFE_ROOT + '/python/four.png'
+IMAGE_FILE = CAFFE_ROOT + '/examples/guchipa/myimg.png'
 
 net = caffe.Classifier(MODEL_FILE, PRETRAINED, image_dims=(28,28), raw_scale=255)
 caffe.set_mode_cpu()
