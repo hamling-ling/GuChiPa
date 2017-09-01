540cea2b4fa6a25a9c7e1cce3642f6269189e240
Nobuhiro Kuroiwa <nobuhiro.kuroiwa@pixela.jp>
2017-09-01 19:48:04 +0900

    stably create trainset

--- research/caffe_examples_guchipa/create_db.sh
+++ research/caffe_examples_guchipa/create_db.sh
@@ -1,4 +1,8 @@
-rm -rf train_leveldb
+rm -rf train_lmdb
+rm -rf test_lmdb
 
-../../build/tools/convert_imageset data/nnsrc/all/ data/nnsrc/list_all.txt train_leveldb 1 -backend leveldb 28 28
+echo creating train_leveldb
+$CAFFE_ROOT/build/tools/convert_imageset data/nnsrc/all/ data/nnsrc/list_all.txt train_lmdb --backend lmdb --shuffle --resize_height 28 --resize_width 28
 
+echo creating test_leveldb
+$CAFFE_ROOT/build/tools/convert_imageset data/nntst/all/ data/nntst/list_all.txt test_lmdb --backend lmdb --resize_height 28 --resize_width 28
--- research/caffe_examples_guchipa/resize.sh
+++ research/caffe_examples_guchipa/resize.sh
@@ -1,18 +1,46 @@
-#cp -r ./data/gam ./data/nnsrc
+# sub dir data/gam or data/tst should exist
+# when arg $1 == t to use data/tst
+
+#rm -rf data/nnsrc;cp -r ~/GitHub/GuChiPa/research/capture/gam data/nnsrc
+#rm -rf data/nntst;cp -r ~/GitHub/GuChiPa/research/capture/tst data/nntst
+
+sync
+sync
+sync
+
+SRC_DIR="./data/nnsrc"
+
+if [ "$1" == "-t" ]; then
+    SRC_DIR="./data/nntst"
+    echo SRC_DIR=$SRC_DIR
+fi
 
 function resizeImage {
+    pushd $SRC_DIR/$1
+    find . -size 0 | xargs --no-run-if-empty rm
     echo grayscalling $1
-    mogrify ./data/nnsrc/$1/*.jpg -depth 8 -colorspace gray
+    mogrify -depth 8 -colorspace gray "*.jpg"
+    
+    sync;sync;sync
+    
     echo resizing $1
-    mogrify -resize 28x28 ./data/nnsrc/$1/*.jpg
-}
+    mogrify -resize 28x28 "*.jpg"
 
+    sync;sync;sync;
+    
+    echo size>10k
+    echo find . -size +30k
+    find . -size +30k
+
+    sync;sync;sync;
+    popd
+}
 
 function listFiles {
     echo listing $1
     pushd .
     # avoid list too long error
-    cd data/nnsrc/$1
+    cd $SRC_DIR/$1
     for entry in `ls *.jpg`; do
 	echo $entry $2 >> ../list_$1.txt
     done
@@ -24,30 +52,39 @@ resizeImage c
 resizeImage g
 resizeImage z
 
-rm data/nnsrc/list*.txt
+rm $SRC_DIR/list*.txt
 
 listFiles "g" 0
 listFiles "c" 1
 listFiles "a" 2
 listFiles "z" 3
 
-rm -rf data/nnsrc/all
-mkdir data/nnsrc/all
+rm -rf $SRC_DIR/all
+mkdir $SRC_DIR/all
 
 echo merging 1/4
-find data/nnsrc/g -name "*.jpg" -print0 | xargs -0 -I{} mv {} data/nnsrc/all/
+find $SRC_DIR/g -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 echo merging 2/4
-find data/nnsrc/c -name "*.jpg" -print0 | xargs -0 -I{} mv {} data/nnsrc/all/
+find $SRC_DIR/c -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 echo merging 3/4
-find data/nnsrc/a -name "*.jpg" -print0 | xargs -0 -I{} mv {} data/nnsrc/all/
+find $SRC_DIR/a -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 echo merging 4/4
-find data/nnsrc/z -name "*.jpg" -print0 | xargs -0 -I{} mv {} data/nnsrc/all/
+find $SRC_DIR/z -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 
-rm -rf data/nnsrc/g
-rm -rf data/nnsrc/c
-rm -rf data/nnsrc/a
-rm -rf data/nnsrc/z
+#rm -rf $SRC_DIR/g
+#rm -rf $SRC_DIR/c
+#rm -rf $SRC_DIR/a
+#rm -rf $SRC_DIR/z
 
-cat data/nnsrc/list_*.txt > data/nnsrc/list_all.txt
-rm data/nnsrc/list_?.txt
+sync;sync;sync;
 
+cat $SRC_DIR/list_*.txt > $SRC_DIR/list_all.txt
+rm $SRC_DIR/list_?.txt
+
+echo resizing $1
+mogrify -resize 28x28 "$SRC_DIR/all/*.jpg"
+sync;sync;sync;
+
+echo size>10k
+echo find $SRC_DIR/all -size +30k
+find $SRC_DIR/all -size +30k
