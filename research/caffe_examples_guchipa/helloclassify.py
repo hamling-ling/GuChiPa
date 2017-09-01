e9a4b4eebac11132a2500d6f233869849dbb495b
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2017-09-02 02:19:47 +0900

    use file given through command line parameter

--- research/caffe_examples_guchipa/helloclassify.py
+++ research/caffe_examples_guchipa/helloclassify.py
@@ -1,14 +1,23 @@
 import caffe
+import os
+import sys
 
 CAFFE_ROOT = os.getenv("CAFFE_ROOT")
 MODEL_FILE = CAFFE_ROOT + '/examples/guchipa/lenet.prototxt'
 PRETRAINED = CAFFE_ROOT + '/examples/guchipa/lenet_iter_10000.caffemodel'
-IMAGE_FILE = CAFFE_ROOT + '/examples/guchipa/myimg.png'
+
+if(len(sys.argv) < 2):
+    print("need image file")
+    exit(1)
+
+image_file = sys.argv[1]
 
 net = caffe.Classifier(MODEL_FILE, PRETRAINED, image_dims=(28,28), raw_scale=255)
-caffe.set_mode_cpu()
+#caffe.set_mode_cpu()
+caffe.set_mode_gpu()
 
-image = caffe.io.load_image(IMAGE_FILE, color=False)
+image = caffe.io.load_image(image_file, color=False)
+print(image.shape)
 
 score = net.predict([image], oversample=False)
 print score
