f19666c18a25966334dd0c253d1342ccef737543
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2017-09-02 02:18:42 +0900

    filan image to png. copy clean files before resize

--- research/caffe_examples_guchipa/resize.sh
+++ research/caffe_examples_guchipa/resize.sh
@@ -1,9 +1,6 @@
 # sub dir data/gam or data/tst should exist
 # when arg $1 == t to use data/tst
 
-#rm -rf data/nnsrc;cp -r ~/GitHub/GuChiPa/research/capture/gam data/nnsrc
-#rm -rf data/nntst;cp -r ~/GitHub/GuChiPa/research/capture/tst data/nntst
-
 sync
 sync
 sync
@@ -13,6 +10,9 @@ SRC_DIR="./data/nnsrc"
 if [ "$1" == "-t" ]; then
     SRC_DIR="./data/nntst"
     echo SRC_DIR=$SRC_DIR
+    rm -rf data/nntst;cp -r ~/GitHub/GuChiPa/research/capture/tst data/nntst
+else
+    rm -rf data/nnsrc;cp -r ~/GitHub/GuChiPa/research/capture/gam data/nnsrc
 fi
 
 function resizeImage {
@@ -24,12 +24,12 @@ function resizeImage {
     sync;sync;sync
     
     echo resizing $1
-    mogrify -resize 28x28 "*.jpg"
+    mogrify -resize 28x28 -format png "*.jpg"
 
     sync;sync;sync;
     
     echo size>10k
-    echo find . -size +30k
+    echo find . -name *.png -size +30k
     find . -size +30k
 
     sync;sync;sync;
@@ -41,7 +41,7 @@ function listFiles {
     pushd .
     # avoid list too long error
     cd $SRC_DIR/$1
-    for entry in `ls *.jpg`; do
+    for entry in `ls *.png`; do
 	echo $entry $2 >> ../list_$1.txt
     done
     popd
@@ -52,7 +52,7 @@ resizeImage c
 resizeImage g
 resizeImage z
 
-rm $SRC_DIR/list*.txt
+rm -f $SRC_DIR/list*.txt
 
 listFiles "g" 0
 listFiles "c" 1
@@ -63,13 +63,13 @@ rm -rf $SRC_DIR/all
 mkdir $SRC_DIR/all
 
 echo merging 1/4
-find $SRC_DIR/g -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
+find $SRC_DIR/g -name "*.png" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 echo merging 2/4
-find $SRC_DIR/c -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
+find $SRC_DIR/c -name "*.png" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 echo merging 3/4
-find $SRC_DIR/a -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
+find $SRC_DIR/a -name "*.png" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 echo merging 4/4
-find $SRC_DIR/z -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
+find $SRC_DIR/z -name "*.png" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 
 #rm -rf $SRC_DIR/g
 #rm -rf $SRC_DIR/c
@@ -79,12 +79,12 @@ find $SRC_DIR/z -name "*.jpg" -print0 | xargs -0 -I{} mv {} $SRC_DIR/all/
 sync;sync;sync;
 
 cat $SRC_DIR/list_*.txt > $SRC_DIR/list_all.txt
-rm $SRC_DIR/list_?.txt
+rm -f $SRC_DIR/list_?.txt
 
 echo resizing $1
-mogrify -resize 28x28 "$SRC_DIR/all/*.jpg"
+mogrify -resize 28x28 "$SRC_DIR/all/*.png"
 sync;sync;sync;
 
-echo size>10k
+echo finding not procecced files
 echo find $SRC_DIR/all -size +30k
 find $SRC_DIR/all -size +30k
