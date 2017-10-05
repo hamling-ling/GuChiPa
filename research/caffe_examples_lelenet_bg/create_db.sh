cb5241cfe8bc2397ccc968c63f7e096938b3a82e
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2017-10-05 22:59:44 +0900

    shuffle db for lelenet_bg

--- research/caffe_examples_lelenet_bg/create_db.sh
+++ research/caffe_examples_lelenet_bg/create_db.sh
@@ -5,4 +5,4 @@ echo creating train_leveldb
 $CAFFE_ROOT/build/tools/convert_imageset data/nnsrc/all/ data/nnsrc/list_all.txt train_lmdb --backend lmdb --shuffle --resize_height 64 --resize_width 64
 
 echo creating test_leveldb
-$CAFFE_ROOT/build/tools/convert_imageset data/nntst/all/ data/nntst/list_all.txt test_lmdb --backend lmdb --resize_height 64 --resize_width 64
+$CAFFE_ROOT/build/tools/convert_imageset data/nntst/all/ data/nntst/list_all.txt test_lmdb --backend lmdb --shuffle --resize_height 64 --resize_width 64
